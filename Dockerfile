248a8b60bdf4850ecebad70041c7bc118fd30db3
Sela <selama@wix.com>
2020-07-27 12:02:54 +0300

    Initial commit from Create Yoshi App

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM docker-repo.wixpress.com/wix-bootstrap-onbuild:stable
+MAINTAINER Sela <selama@wix.com>
---
+++ src/external-types.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="yoshi/types" />
+/// <reference types="jest-yoshi-preset/types" />
+/// <reference types="@wix/wix-bootstrap-renderer/declarations" />
+
+interface Window {
+  __LOCALE__: string;
+  __BASEURL__: string;
+  __MESSAGES__: Messages;
+}
+
+// tslint:disable-next-line:no-namespace
+declare namespace Express {
+  interface Request {
+    aspects: any;
+  }
+}
+
+declare module 'yoshi-template-intro';
+declare module 'wix-node-i18n-cache';
